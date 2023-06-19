e06ab685832f249221e0d6aabfbe79faf8d622fa
DmitryTell <dmitrij.teleganov@gmail.com>
2023-06-19 20:06:57 +0300

    Screen-keyboard layout complete

--- Экранная-клавиатура/js/render.js
+++
@@ -1,6 +0,0 @@
-const renderKeyboard = (element, arr1, arr2, arr3, arr4, arr5) => {
-    element.innerHTML = arr1.map(el => {
-        return `<button class="key ${el.isSpecial ? '-special -3x' : '-sample'}" 
-        id="${el.isSpecial ? el.id : ''}">${el.name}</button>`;
-    }).join('');
-}
